c9d5c40d6f409cbe6af009a8d18473e3b8a6538d
Pooya Nasiri <nasiripooya254@gmail.com>
2025-03-29 14:42:43 +0330

    add layout

---
+++ positivus/src/Pricing.jsx
@@ -0,0 +1,6 @@
+function Pricing() {
+    return(
+        <h1>Pricing</h1>
+    )
+}
+export default Pricing
---
+++ positivus/src/UseCases.jsx
@@ -0,0 +1,6 @@
+function UseCases() {
+    return(
+        <h1>UseCases</h1>
+    )
+}
+export default UseCases
